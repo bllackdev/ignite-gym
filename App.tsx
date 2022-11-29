3bea233def9bf1a9eee45b9c510db749a53a6c68
Raphael Renato <rapha_rmm@hotmail.com>
2022-11-29 20:15:11 -0300

    💄 Add theme

--- App.tsx
+++ App.tsx
@@ -5,19 +5,22 @@ import {
   Roboto_400Regular,
   Roboto_700Bold,
 } from "@expo-google-fonts/roboto";
+
+import {THEME} from "./src/theme";
+
 import { Loading } from "./src/components/Loading";
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
 
   return (
-    <NativeBaseProvider>
+    <NativeBaseProvider theme={THEME}>
       <StatusBar
         barStyle="light-content"
         backgroundColor="transparent"
         translucent
       />
-      {fontsLoaded ? <Center>Hello world</Center> : <Loading />}
+      {fontsLoaded ? <Box /> : <Loading />}
     </NativeBaseProvider>
   );
 }
